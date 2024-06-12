8b49b62353085b2dc19775560711a689162731bc
polya001 <99148202+polya001@users.noreply.github.com>
2024-06-12 16:17:50 +0300

    Delete tests/test_format.py

--- tests/test_format.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-def test_code_formatting():
-    with open('calc_functions.py') as file:
-        code = file.read()
-    formatted_code = subprocess.check_output(['black', '-', '--quiet', '-'], input=code.encode()).decode()
-    assert code == formatted_code
